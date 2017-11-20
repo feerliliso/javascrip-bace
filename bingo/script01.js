ec7c6049c3c7f445d544b2c9d57892c1c3849a03
feerliliso <feerliliso@hotmail.com>
2017-11-20 16:57:58 +0800

    add new faunction
    add new faunction

--- bingo/script01.js
+++ bingo/script01.js
@@ -2,12 +2,17 @@ window.onload = initAll;
 var usedNums = new Array(76);
 function initAll() {
   if(document.getElementById){
-  for (var i = 0; i<24; i++) {
-   setSquare(i);
-  }
+    document.getElementById("reload").onclick = anotherCard;
+    newCard();
 }else {
   alert("Sorry, your browser doesn`t support this script.");
+   }
 }
+  function newCard() {
+    for (var i = 0; i<24; i++) {
+     setSquare(i);
+    }
+  }
   function setSquare(thisSquare) {
   var currSquare = "square" + thisSquare;
   var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
@@ -17,16 +22,16 @@ function initAll() {
     newNum = colBasis+getNewNum()+1;
   } while (usedNums[newNum]);
   usedNums[newNum] = true;
-  //var newNum = colBasis + getNewNum() + 1;
 
-   /*if (!usedNums[newNum]) {
-     usedNums[newNum] = true;*/
-
-  //var newNum = Math.floor(Math.random() * 75)+ 1;
   document.getElementById(currSquare).innerHTML = newNum;
    }
-
-}
 function getNewNum() {
     return Math.floor(Math.random()*15);
+}
+function anotherCard() {
+  for (var i= 1; i<usedNums.length; i++){
+    usedNums[i] = false;
+  }
+  newCard();
+  return false;
 }
